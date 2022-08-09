ec45a57c5ea547ca5b2cfa121ce69e171ec59c60
KiteCaptain <97241357+KiteCaptain@users.noreply.github.com>
2022-08-09 04:21:07 +0300

    modifed letter_finder to handle errors

--- letter_finder.js
+++ letter_finder.js
@@ -2,13 +2,28 @@
  Function which will be able to take a word and locate the position of a chosen letter in that given word.  
  */
 function letterFinder(word, match){
-    for (var i = 0; i < word.length; i++){
-        if(word[i] == match) {
-            console.log("Found the letter ",match, " at ", i)
+    try {
+        if (word.length < 2){
+            console.log("The word is too short!");
+            throw new ReferenceError;
+        } else if (match.length > 1){
+            console.log("The match letter is too long!");
+            throw new ReferenceError;
         } else {
-            console.log("...No match found at ", i)
-        }
+            for (var i = 0; i < word.length; i++){
+                if(word[i] == match) {
+                    console.log("Found the letter ",match, " at ", i);
+                } else {
+                    console.log("...No match found at ", i);
+                }
+            }
+        }     
     }
+   catch (errors){
+    console.log("The error is a:", errors)
+   }
 }
 
-letterFinder("Eugine", "i")
+letterFinder("Eugine", "yi")
+
+console.log("...>>> Moving on >>>")
